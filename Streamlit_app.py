88bb4292317577e79dba87a68512f1a6770bfd4b
Mangaiyarkarasi Chandrasekar <mangaikmc3@gmail.com>
2024-12-30 15:51:26 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,4 +4,18 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-  
+# Streamlit UI
+st.title("Mulitiple Disease Prediction")
+
+# Sidebar for disease selection
+nav = st.sidebar.radio("Select Multiple Disease Prediction", ["Parkinson's Disease", "Kidney Disease", "Liver Disease"])
+
+if nav == "Parkinson's Disease":
+    st.header("Parkinson's Multiple Disease Prediction")
+    
+    # Load the Parkinson's model
+    try:
+        parkinson_model = pickle.load(open(r'XGBparkinsons.pkl', 'wb'))
+    except FileNotFoundError:
+        st.error("Model file not found. Please check the file path.")
+        st.stop()
