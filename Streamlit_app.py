27c2db04c8f8d62283f556f90dbccac2bd7771af
Mangaiyarkarasi Chandrasekar <mangaikmc3@gmail.com>
2024-12-30 19:41:37 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -129,7 +129,7 @@ elif nav == "Liver Disease":
 
     # Load the liver's model
     try:
-        liver_model = pickle.load(open('DTLiver.pkl','rb'))
+        liver_model = pickle.load(open('https://github.com/Mangai2024/Multiple_Disease_Prediction/blob/main/LRCliver.pkl','rb'))
     except FileNotFoundError:
         st.error("Model file not found. Please check the file path.")
         st.stop()    
